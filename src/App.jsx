d42c2de4056c2d56609f8b091d1e1b8f7a5fc7b4
Nishan <nishankrishna21@gmail.com>
2026-02-17 23:19:49 +0530

    Add Resources page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 import Dashboard from "./pages/Dashboard";
+import Simulator from "./pages/Simulator";
+import About from "./pages/About";
 import Cpu from "./pages/CPU";
 import Memory from "./pages/Memory";
 import PageSim from "./pages/Page";
+
+/* ADD THIS IMPORT */
+import Resources from "./pages/Resources";
 
 function App() {
   return (
@@ -18,11 +23,18 @@ function App() {
 
         <Route path="/" element={<Dashboard />} />
 
+        <Route path="/simulator" element={<Simulator />} />
+
+        <Route path="/about" element={<About />} />
+
         <Route path="/cpu" element={<Cpu />} />
 
         <Route path="/memory" element={<Memory />} />
 
         <Route path="/page" element={<PageSim />} />
+
+        {/* ADD THIS ROUTE */}
+        <Route path="/resources" element={<Resources />} />
 
       </Routes>
     </>
